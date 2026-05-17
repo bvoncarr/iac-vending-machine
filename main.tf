ff47996400c92e0b4b370fd222cfb1e7978a0479
bvoncarr <b.voncarr@gmail.com>
2026-05-17 13:50:04 +0700

    add backend credentials

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "bvoncarrtfstate1"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
